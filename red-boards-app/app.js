728cce6cbee2def10b197449b275909f655454bf
Calvin Irwin <cirwin@lwolf.com>
2015-11-09 21:22:05 -0500

    connected the list view to the details view

--- red-boards-app/app.js
+++ red-boards-app/app.js
@@ -6,23 +6,21 @@ app.config([
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
-
     $stateProvider
       .state('home', {
         url: '/home',
         templateUrl: '/data_list.html',
         controller: 'mainCtrl'
-        // ,
-        // resolve: {
-        //   postPromise: ['exItems', function(exceptions){
-        //     return exItems.getAll();
-        //   }]
-        // }
       })
       .state('item', {
         url: '/item/{id}',
         templateUrl: '/item.html',
-        controller: 'itemCtrl'
+        controller: 'itemCtrl',
+        resolve: {
+          item: ['$stateParams', 'exItems', function($stateParams, exItems) {
+            return exItems.get($stateParams.id);
+          }]
+        }
       });
     $urlRouterProvider.otherwise('home');
   }
@@ -32,69 +30,6 @@ app.config([
 app.factory('exItems', ['$http', function($http) {
   var o = {
     items: []
-    // [{
-    //     mls_area: "EBRDX",
-    //     exception_type: "photo",
-    //     client_code: "AME717",
-    //     client_name: "A. Meadows Real Estate",
-    //     last_run: "10/20/2015 6:24:46 PM",
-    //     host_server: "PHOTOSRV1",
-    //     status: "New",
-    //     create_date: "10/22/2015 8:05:00 PM",
-    //     author: "system",
-    //     body: "The image failed to be retrieved from the server",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "EBRDI",
-    //     exception_type: "data",
-    //     client_code: "AME717",
-    //     client_name: "A. Meadows Real Estate",
-    //     last_run: "10/20/2015 6:24:46 PM",
-    //     host_server: "MLS4",
-    //     status: "New",
-    //     create_date: "10/22/2015 8:00:00 PM",
-    //     author: "system",
-    //     body: "Could not connect to remote server",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "REBC",
-    //     exception_type: "data",
-    //     client_code: "REM001",
-    //     client_name: "Hespeler Rd Remax",
-    //     last_run: "10/19/2015 5:01:03 PM",
-    //     host_server: "MLS1",
-    //     status: "New",
-    //     create_date: "10/22/2015 9:00:00 PM",
-    //     author: "system",
-    //     body: "Credentials failed",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "REKW",
-    //     exception_type: "data",
-    //     client_code: "ROY012",
-    //     client_name: "Royal lePage Kitchener",
-    //     last_run: "10/21/2015 3:14:35 PM",
-    //     host_server: "MLS1",
-    //     status: "New",
-    //     create_date: "10/22/2015 9:01:00 PM",
-    //     author: "system",
-    //     body: "Credentials failed",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   },
-    //
-    // ]
   };
 
   o.get = function(id) {
@@ -110,11 +45,7 @@ app.factory('exItems', ['$http', function($http) {
   };
 
   o.getAll = function() {
-    //console.log("getting all... " + DATA_URL + 'mlsexceptions/');
-    return $http.get(DATA_URL + 'mlsexceptions/'); //.then(function(res) {
-      //console.log(res.data);
-    //   return res.data;
-    // });
+    return $http.get(DATA_URL + 'mlsexceptions/');
   };
 
   return o;
@@ -126,114 +57,35 @@ app.controller("mainCtrl", [
   function($scope, exItems) {
     $scope.title = "red-board manager";
     exItems.getAll().then(function(res) {
-      $scope.data =  res.data;
+      $scope.data = res.data;
     }).catch(function(err, res) {
-      $scope.data =  [{
-          mls_area: "EBRDX",
-          exception_type: "photo",
-          client_code: "AME717",
-          client_name: "A. Meadows Real Estate",
-          last_run: "10/20/2015 6:24:46 PM",
-          host_server: "PHOTOSRV1",
-          status: "New",
-          create_date: "10/22/2015 8:05:00 PM",
-          author: "system",
-          body: "The image failed to be retrieved from the server",
-          cause: "",
-          ticket_num: "",
-          action_taken: "",
-          comments: []
-        }];
-
-
-
+      // this sample data only loads when the db connection is down
+      $scope.data = [{
+        mls_area: "SAMPLE",
+        exception_type: "datafeed",
+        client_code: "SAM999",
+        client_name: "A. Peaceful Real Estate",
+        last_run: "10/20/2015 6:24:46 PM",
+        host_server: "SERVER1",
+        status: "New",
+        create_date: "11/01/2015 8:05:00 PM",
+        author: "system",
+        body: "The data could not be retrieved from the server",
+        cause: "",
+        ticket_num: "",
+        action_taken: "",
+        comments: []
+      }];
     });
-    // [{
-    //     mls_area: "EBRDX",
-    //     exception_type: "photo",
-    //     client_code: "AME717",
-    //     client_name: "A. Meadows Real Estate",
-    //     last_run: "10/20/2015 6:24:46 PM",
-    //     host_server: "PHOTOSRV1",
-    //     status: "New",
-    //     create_date: "10/22/2015 8:05:00 PM",
-    //     author: "system",
-    //     body: "The image failed to be retrieved from the server",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "EBRDI",
-    //     exception_type: "data",
-    //     client_code: "AME717",
-    //     client_name: "A. Meadows Real Estate",
-    //     last_run: "10/20/2015 6:24:46 PM",
-    //     host_server: "MLS4",
-    //     status: "New",
-    //     create_date: "10/22/2015 8:00:00 PM",
-    //     author: "system",
-    //     body: "Could not connect to remote server",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "REBC",
-    //     exception_type: "data",
-    //     client_code: "REM001",
-    //     client_name: "Hespeler Rd Remax",
-    //     last_run: "10/19/2015 5:01:03 PM",
-    //     host_server: "MLS1",
-    //     status: "New",
-    //     create_date: "10/22/2015 9:00:00 PM",
-    //     author: "system",
-    //     body: "Credentials failed",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   }, {
-    //     mls_area: "REKW",
-    //     exception_type: "data",
-    //     client_code: "ROY012",
-    //     client_name: "Royal lePage Kitchener",
-    //     last_run: "10/21/2015 3:14:35 PM",
-    //     host_server: "MLS1",
-    //     status: "New",
-    //     create_date: "10/22/2015 9:01:00 PM",
-    //     author: "system",
-    //     body: "Credentials failed",
-    //     cause: "",
-    //     ticket_num: "",
-    //     action_taken: "",
-    //     comments: []
-    //   },
-
-    // ];
   }
 ]);
 
 app.controller("itemCtrl", [
   '$scope',
-  function($scope) {
+  'exItems',
+  'item',
+  function($scope, exItems, item) {
     $scope.title = "red-board manager";
-    //$scope.item = //{};
-    $scope.item = {
-        mls_area: "EBRDX",
-        exception_type: "photo",
-        client_code: "AME717",
-        client_name: "A. Meadows Real Estate",
-        last_run: "10/20/2015 6:24:46 PM",
-        host_server: "PHOTOSRV1",
-        status: "New",
-        create_date: "10/22/2015 8:05:00 PM",
-        author: "system",
-        body: "The image failed to be retrieved from the server",
-        cause: "",
-        ticket_num: "",
-        action_taken: "",
-        comments: []
-      };
-     }
+    $scope.item = item;
+  }
 ]);
